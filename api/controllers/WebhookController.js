9b95467f9a9f445a94c324160efea8b7e4fd1145
Cezar <grigorecezar@yahoo.com>
2016-05-06 21:08:44 +0100

    webhookpost

--- api/controllers/WebhookController.js
+++ api/controllers/WebhookController.js
@@ -9,12 +9,12 @@ module.exports = {
     webhookpost: function (req, res) {
     	messaging_events = req.body.entry[0].messaging;
         for (i = 0; i < messaging_events.length; i++) {
-          event = req.body.entry[0].messaging[i];
-          sender = event.sender.id;
-          if (event.message && event.message.text) {
-            text = event.message.text;
-            // Handle a text message from this sender
-          }
+           event = req.body.entry[0].messaging[i];
+           sender = event.sender.id;
+           if (event.message && event.message.text) {
+               text = event.message.text;
+               // Handle a text message from this sender
+           }
         }
         res.sendStatus(200);
     }
