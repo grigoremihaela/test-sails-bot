9c23701ad567464149c600bd57a6d44e35266585
Cezar <grigorecezar@yahoo.com>
2016-05-06 22:14:40 +0100

    webhookpost add

--- api/controllers/WebhookController.js
+++ api/controllers/WebhookController.js
@@ -1,12 +1,25 @@
 module.exports = {
-  webhook: function (req, res) {
-    if (req.query['hub.verify_token'] === 'test-sails-bot') {
-       res.send(req.query['hub.challenge']);
-    }
-    res.send('Error, wrong validation token');
-    }
 
-    
+    webhook: function (req, res) {
+        if (req.query['hub.verify_token'] === 'test-sails-bot') {
+           res.send(req.query['hub.challenge']);
+        }
+        res.send('Error, wrong validation token');
+    },
+
+    webhookpost: function (req, res) {
+    	messaging_events = req.body.entry[0].messaging;
+        for (i = 0; i < messaging_events.length; i++) {
+            event = req.body.entry[0].messaging[i];
+            sender = event.sender.id;
+            if (event.message && event.message.text) {
+              text = event.message.text;
+              // Handle a text message from this sender
+            }
+        }
+        res.sendStatus(200);
+    }
 
 };
+
 
