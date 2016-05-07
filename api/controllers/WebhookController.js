ff2478d0ba6fa3ef172c2db6bffeccedb40c0798
Cezar <grigorecezar@yahoo.com>
2016-05-07 18:32:33 +0100

    answer message ok

--- api/controllers/WebhookController.js
+++ api/controllers/WebhookController.js
@@ -16,12 +16,11 @@ module.exports = {
               text = event.message.text;
               // Handle a text message from this sender
             }
-        }
-        Webhook.create({sender:sender, text:text}).exec(function createCB(err, created){
-            console.log('Created text' + created.text);
-        });
-        FacebookService.sendTextMessage(sender, "Mesaj primit!");
-       
+            Webhook.create({sender:sender, text:text}).exec(function createCB(err, created){
+                console.log('Created text' + created.text);
+            });
+            FacebookService.sendTextMessage(sender, "Mesaj primit!");
+        }             
 //        FacebookService.sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
         res.send(200, 'Ok!');
     },
